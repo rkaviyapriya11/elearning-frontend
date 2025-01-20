b6fcb31a7f039ab34bb8436bb86a118e6f17e5e4
rkaviyapriya <rkaviyapriya11@gmail.com>
2025-01-20 11:07:44 +0530

    api update

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx';
 import { UserContextProvider } from './contextapi/UserContext.jsx';
 import { CourseContextProvider } from './contextapi/CourseContext.jsx';
 
-export const server = 'http://localhost:5000';
+export const server = 'https://elearning-backend-1no3.onrender.com';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
